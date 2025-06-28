a6f459c0a958a53a3c90d64b156e80816753670b
aaron <xiaofengliu1992@hotmail.com>
2025-06-27 20:59:31 -0400

    hide scrollbar

--- src/pages/Layout/elements.ts
+++ src/pages/Layout/elements.ts
@@ -7,4 +7,10 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   overflow: auto;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, and Opera */
+  }
 `;
