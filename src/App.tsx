22298a8f184b719b3b43b9adc6fe97029b8f35d3
Aaron <xiaofengliu1992@hotmail.com>
2025-07-15 02:19:20 -0400

    apply lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import ReservationStepper from "./components/ReservatioinStepper/ReservationStepper";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import { lazy, Suspense } from "react";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import HomeNav from "./components/HomeNav/HomeNav";
 import AuthInitializer from "./components/AuthInitializer/AuthInitializer";
-import EmployeeCalendar from "./pages/Calendar";
-import ReservationDetail from "./pages/Reservation/ReservationDetail";
+
+const SignIn = lazy(() => import("./pages/Auth/SignIn"));
+const SignUp = lazy(() => import("./pages/Auth/SignUp"));
+const ReservationStepper = lazy(
+  () => import("./components/ReservatioinStepper/ReservationStepper")
+);
+const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
+const EmployeeCalendar = lazy(() => import("./pages/Calendar"));
+const ReservationDetail = lazy(
+  () => import("./pages/Reservation/ReservationDetail")
+);
 
 const App: React.FC = () => {
   return (
     <AuthInitializer>
       <Router>
         <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/booknow" element={<ReservationStepper />} />
+          <Route
+            path="/signin"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <SignIn />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <SignUp />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/booknow"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ReservationStepper />
+              </Suspense>
+            }
+          />
           <Route path="/" element={<HomeNav />} />
           <Route
             path="/dashboard"
             element={
               <ProtectedRoute>
-                <Dashboard />
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Dashboard />
+                </Suspense>
               </ProtectedRoute>
             }
           />
@@ -30,11 +59,20 @@ const App: React.FC = () => {
             path="/calendar"
             element={
               <ProtectedRoute>
-                <EmployeeCalendar />
+                <Suspense fallback={<div>Loading...</div>}>
+                  <EmployeeCalendar />
+                </Suspense>
               </ProtectedRoute>
             }
           />
-          <Route path="/reservation/:id" element={<ReservationDetail />} />
+          <Route
+            path="/reservation/:id"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ReservationDetail />
+              </Suspense>
+            }
+          />
         </Routes>
       </Router>
     </AuthInitializer>
